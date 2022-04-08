70fc63e4bf63edcae4085fec613878d0cb988d21
猪小乐 <zhuxiaole@zhuxiaole.org>
2022-04-09 02:35:06 +0800

    修改访客网络翻译问题

--- diy/Modify.sh
+++ diy/Modify.sh
@@ -3,6 +3,8 @@ sed -i \
 -e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' \
 */Makefile
 
+sed -i 's|msgstr"创建/删除"|msgstr "创建/删除"|g' luci-app-guest-wifi/po/zh-cn/guest-wifi.po
+
 bash diy/convert_translation.sh -a >/dev/null 2>&1
 
 exit 0
