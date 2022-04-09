5409e8132818f07bfe57b5b79a20df7bb87b3a95
猪小乐 <zhuxiaole@zhuxiaole.org>
2022-04-10 01:08:15 +0800

    自定义argon主题logo

--- diy/Modify.sh
+++ diy/Modify.sh
@@ -5,6 +5,9 @@ sed -i \
 
 sed -i 's|msgstr"创建/删除"|msgstr "创建/删除"|g' luci-app-guest-wifi/po/zh-cn/guest-wifi.po
 
+rm -rf luci-theme-argon/htdocs/luci-static/argon/img/argon.svg
+cp diy/argon.svg luci-theme-argon/htdocs/luci-static/argon/img/
+
 bash diy/convert_translation.sh -a >/dev/null 2>&1
 
 exit 0
