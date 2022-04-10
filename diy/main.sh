009c6a50ea0b4b545ffac6c5a2ccb870fc100777
猪小乐 <zhuxiaole@zhuxiaole.org>
2022-04-10 16:55:46 +0800

    添加软件包

--- diy/main.sh
+++ diy/main.sh
@@ -16,6 +16,13 @@ rm -rf $1
 }
 
 git clone --depth 1 https://github.com/destan19/OpenAppFilter && mvdir OpenAppFilter
+git clone --depth 1 https://github.com/sirpdboy/luci-app-advanced
+git clone --depth 1 https://github.com/tty228/luci-app-serverchan
+git clone --depth 1 https://github.com/ntlf9t/luci-app-easymesh
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config
+git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall passwall-pkgs && rm -rf passwall-pkgs/.github && mv -n passwall-pkgs/* ./ && rm -rf passwall-pkgs
+git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall passwall-luci && mv -n passwall-luci/luci-app-passwall ./ && rm -rf passwall-luci
 
 svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-wrtbwmon
 svn co https://github.com/coolsnowwolf/packages/trunk/net/uugamebooster
