bf443c43708267fbb3208c1a525567f45cc2fd88
猪小乐 <zhuxiaole@zhuxiaole.org>
2022-04-16 02:30:20 +0800

    修改路径错误

--- diy/Modify.sh
+++ diy/Modify.sh
@@ -3,8 +3,8 @@ sed -i \
 -e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' \
 */Makefile
 
-rm -rf applications/luci-app-guest-wifi/po/zh_Hans/guest-wifi.po
-mv applications/luci-app-guest-wifi/po/zh_Hans/luci-app-guest-wifi.po applications/luci-app-guest-wifi/po/zh_Hans/guest-wifi.po
+rm -rf luci-app-guest-wifi/po/zh_Hans/guest-wifi.po
+mv luci-app-guest-wifi/po/zh_Hans/luci-app-guest-wifi.po luci-app-guest-wifi/po/zh_Hans/guest-wifi.po
 
 bash diy/convert_translation.sh -a >/dev/null 2>&1
 
