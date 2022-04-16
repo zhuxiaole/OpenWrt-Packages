f3eeb0021087d9bf5ec5c7de85278edd4c5619e2
猪小乐 <zhuxiaole@zhuxiaole.org>
2022-04-16 16:10:39 +0800

    修复dnsproxy依赖golang问题

--- diy/Modify.sh
+++ diy/Modify.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sed -i \
--e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' \
-*/Makefile
+sed -i -e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' */Makefile
+sed -i -e 's?\.\./\.\./lang/golang/golang-package.mk?$(TOPDIR)/feeds/packages/lang/golang/golang-package.mk?' */Makefile
 
 rm -rf luci-app-guest-wifi/po/zh_Hans/guest-wifi.po
 mv luci-app-guest-wifi/po/zh_Hans/luci-app-guest-wifi.po luci-app-guest-wifi/po/zh_Hans/guest-wifi.po
