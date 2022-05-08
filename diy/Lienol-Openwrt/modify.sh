0609f2d890aa3ac064020fde3744db92e7b6f637
猪小乐 <1365441936@qq.com>
2022-05-08 11:45:40 +0800

    fix error

--- diy/Lienol-Openwrt/modify.sh
+++ diy/Lienol-Openwrt/modify.sh
@@ -5,6 +5,8 @@ sed -i -e 's?\.\./\.\./lang/golang/golang-package.mk?$(TOPDIR)/feeds/packages/la
 mkdir -p luci-app-wizard/patches
 cp $GITHUB_WORKSPACE/diy/Lienol-Openwrt/patches/luci-app-wizard/* luci-app-wizard/patches
 
+sed -i 's|PKG_MAINTAINER:=<https://github.com/Ausaci/luci-app-nat6-helper>|PKG_MAINTAINER:=Ausaci|g' luci-app-nat6-helper/Makefile
+
 bash $GITHUB_WORKSPACE/diy/common/convert_translation.sh -a >/dev/null 2>&1
 
 echo `date +"%Y-%m-%d %H:%M:%S"` > version
