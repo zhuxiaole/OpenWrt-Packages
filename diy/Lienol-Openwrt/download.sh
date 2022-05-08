68035f08a200c06fb9f8471e1f2b91e29af1bbfd
猪小乐 <1365441936@qq.com>
2022-05-08 10:57:19 +0800

    update packages

--- diy/Lienol-Openwrt/download.sh
+++ diy/Lienol-Openwrt/download.sh
@@ -20,7 +20,7 @@ git clone --depth 1 https://github.com/sirpdboy/luci-app-advanced
 git clone --depth 1 https://github.com/Ausaci/luci-app-nat6-helper
 git clone --depth 1 https://github.com/tty228/luci-app-serverchan
 git clone --depth 1 https://github.com/ntlf9t/luci-app-easymesh
-git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall passwall-pkgs && rm -rf passwall-pkgs/.github && mv -n passwall-pkgs/* ./ && rm -rf passwall-pkgs
+git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall passwall-pkgs && rm -rf passwall-pkgs/.github && mv -n passwall-pkgs/* ./ && rm -rf passwall-pkgs && rm -rf xray-core
 git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall passwall-luci && mv -n passwall-luci/luci-app-passwall ./ && rm -rf passwall-luci
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config
 
@@ -29,8 +29,6 @@ git_sparse_clone master "https://github.com/kiddin9/openwrt-packages" "wizard_lu
 git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/luci" "eqos_luci" applications/luci-app-eqos
 git_sparse_clone master "https://github.com/sundaqiang/openwrt-packages" "wolplus_luci" luci-app-wolplus
 git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/luci" "accesscontrol_luci" applications/luci-app-accesscontrol
-git_sparse_clone main "https://github.com/kenzok8/small-package" "adguardhome_pkg" adguardhome
-git_sparse_clone main "https://github.com/kenzok8/small-package" "adguardhome_luci" luci-app-adguardhome
 
 rm -rf ./*/.git & rm -f ./*/.gitattributes
 rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
