db0f3a7f28c78b5d6a4a04e9c7a9ca92e3b8a0ac
猪小乐 <1365441936@qq.com>
2022-05-08 11:42:02 +0800

    fix error

--- diy/Lienol-Openwrt/modify.sh
+++ diy/Lienol-Openwrt/modify.sh
@@ -3,7 +3,7 @@ sed -i -e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' */Makefile
 sed -i -e 's?\.\./\.\./lang/golang/golang-package.mk?$(TOPDIR)/feeds/packages/lang/golang/golang-package.mk?' */Makefile
 
 mkdir -p luci-app-wizard/patches
-cp $GITHUB_WORKSPACE/diy/lienol-op/patches/luci-app-wizard/* luci-app-wizard/patches
+cp $GITHUB_WORKSPACE/diy/Lienol-Openwrt/patches/luci-app-wizard/* luci-app-wizard/patches
 
 bash $GITHUB_WORKSPACE/diy/common/convert_translation.sh -a >/dev/null 2>&1
 
